ba4a999e37d7a9ddeebc23f6fe1e7fcb6fefce15
The1024thmember <1786728683@qq.com>
2024-04-09 23:42:18 +1000

    modify the title to contain the method used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: ` <div style="text-align:center">
-      <h1>Welcome to A Non-Reactive Shopping Cart</h1>
+      <h1>Welcome to A Reactive Shopping Cart (using subject)</h1>
     </div>
     <app-product-list class="products-container"></app-product-list>
     <app-cart-summary class="cart-summary"></app-cart-summary>`,
